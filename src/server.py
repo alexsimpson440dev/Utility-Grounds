c0713afe9853c8784a7f11c5c003f37ddcd2e3e4
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-10-31 00:14:03 -0500

    added some files for heroku.

--- src/server.py
+++ src/server.py
@@ -1,1 +1,8 @@
-from flask import render_template, redirect, request
+from flask import Flask, render_template, redirect, request
+app = Flask(__name__, '/static', static_folder='../static', template_folder='../templates')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()

88074715cbe6ff172ddf1a2f2aaa85a0d204e791
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-11-06 21:14:19 -0600

    added db boilerplate

--- src/server.py
+++ src/server.py
@@ -1,5 +1,11 @@
+import os
 from flask import Flask, render_template, redirect, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__, '/static', static_folder='../static', template_folder='../templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')

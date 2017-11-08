697305b61828d941838123d8427c62cb11814570
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-11-07 20:26:40 -0600

    testing moving flask to app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from src.server import app
+#from src.server import app
+import os
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, '/static', static_folder='../static', template_folder='../templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"postgresql://postgres:password/grounds")
+db = SQLAlchemy(app)
 
 if __name__=='__main__':
     app.run()

c931b9a1fa5cae8e681f51c418b6a716ed6451df
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-11-13 20:53:28 -0600

    switched to sqlite. Added basic functions to database such as adding user via the website. Need to add password hashing and so on. Not pretty.

--- src/database.py
+++ src/database.py
@@ -1,14 +1,36 @@
-import os
-from urllib import parse
-import psycopg2
+import sqlite3
+from src.user import User
+from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import mapper, sessionmaker, relationship
+METADATA = MetaData()
 
-parse.uses_netloc.append("postgres")
-url = parse.urlparse(os.environ["DATABASE_URL"])\
+class Database():
+    def __init__(self, connection_string="sqlite:///groundsDB.sqlite3"):
+        self.sql_file = connection_string
+        self.users = self._map_user()
+        self.engine = self._get_connection()
+        METADATA.create_all(self.engine)
 
-conn = psycopg2.connect(
-    database = url.path[1:],
-    user = url.username,
-    password = url.password,
-    host = url.hostname,
-    port = url.port
-)
+    def _map_user(self):
+        users = Table('Users', METADATA,
+                     Column('user_id', Integer, primary_key=True),
+                     Column('first_name', String(30)),
+                     Column('last_name', String(30)),
+                     Column('email_address', String(50)),
+                     Column('password', String(15))
+                     )
+        mapper(User, users)
+        return users
+
+    def _get_connection(self):
+        engine = create_engine(self.sql_file)
+        return engine
+
+    def _get_session(self):
+        session = sessionmaker(bind=self.engine)
+        return session()
+
+    def _add_user(self, user):
+        session = self._get_session()
+        session.add(user)
+        session.commit()
--- src/server.py
+++ src/server.py
@@ -1,14 +1,40 @@
-import os
-from flask import Flask, render_template, redirect, request
-from flask_sqlalchemy import SQLAlchemy
+from src.dbManager import DBManager
+from flask import Flask, render_template, redirect, request, session
+
+MANAGER = DBManager()
 
 app = Flask(__name__, '/static', static_folder='../static', template_folder='../templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
+app.secret_key = 'the moo goes cow'
 
 @app.route('/')
+@app.route('/index')
+@app.route('/index.html', methods=["GET"])
 def index():
     return render_template('index.html')
 
+@app.route('/signup', methods=["GET", "POST"])
+@app.route('/signup.html', methods=["GET"])
+def signup():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email_address = request.form.get('email_address')
+        password = request.form.get('password')
+
+        try:
+            MANAGER.add_user(first_name, last_name, email_address, password)
+            sign_in(email_address)
+
+        except RuntimeError as e:
+            print('Run Time Error: ', e)
+            return redirect('signup.html')
+
+        return render_template('index.html')
+    else:
+        return render_template("signup.html")
+
+def sign_in(email_address):
+    session['email'] = email_address
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=9999, debug=True)

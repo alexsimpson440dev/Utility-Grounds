0659c0bfefa49dd2c002f147cfd83d76adb6f392
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-12-06 20:51:20 -0600

    added some comments to the server.

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 from src.dbManager import DBManager
-import os, datetime
-from flask import Flask, render_template, redirect, request, session, url_for
+import os
+from flask import Flask, render_template, redirect, request, session, flash
 
 MANAGER = DBManager()
 
@@ -38,9 +38,13 @@ def signup():
     else:
         return render_template('signup.html')
 
+# logs a user in
 @app.route('/login', methods=['post', 'get'])
 @app.route('/login.html', methods=['get'])
 def user_login():
+    # gets users info from the html page
+    # the server checks to see if the users credentials are correct
+    # if auth is true the user will be signed in
     if request.method == 'POST':
         email_address = request.form.get('email_address')
         password = request.form.get('password')
@@ -49,9 +53,11 @@ def user_login():
             print("login success")
             sign_in(email_address)
             return redirect("index.html")
+        # if the credentials are wrong
+        # the user will be redirected back to the sign in page
         else:
-            print("email or password is incorrect")
-            return render_template("login.html")
+            flash('email or password is incorrect')
+            return redirect("login.html")
 
     else:
         return render_template("login.html")
@@ -72,23 +78,31 @@ def add_bill():
         total = float(electricity) + float(gas) + float(internet) + float(city)
         due_date = request.form.get('due_date')
 
-
+        # tries to add a bill to the database
+        # if it is a success, the bill will be added and the page will redirect back to the manage.html
+        # this will then show an updated view of the bills
         try:
             MANAGER.add_bill(date_added, electricity, gas, internet, city, total, due_date)
             return redirect("manage.html")
 
+        # if the add fails, this will catch the error and redirect the user back to the manage.html page
         except RuntimeError:
             print('cannot add bill')
             return redirect("manage.html")
 
+    # if the request method is post, the server will get the bills and add them to the html table
     if request.method == 'GET':
         bills = MANAGER._get_bills()
         return render_template("manage.html", bills = bills)
 
+# signs a user in based on the email address
+# todo: add a time out
 def sign_in(email_address):
     session['email'] = email_address
     print('signing in: ' + session['email'])
 
+# if the user chooses to sign out or gets timed out
+# the application will delete the session linked to the users email
 @app.route('/logout', methods=['post', 'get'])
 def sign_out():
     print('signing out: ' + session['email'])
